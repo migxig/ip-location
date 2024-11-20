800f17ebe30676a7d707809fbf1e9e628d3fe1fc
chenmingxing <chenmingxin@3k.com>
2024-11-20 15:06:21 +0800

    feature:将ipip-data的代码搬过来使用

---
+++ ipip/IPTool.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Mingxing.chen
+ * @date 2024/11/20 14:13
+ */
+declare(strict_types=1);
+
+namespace ipip;
+
+
+use ipip\db\Reader;
+
+class IPTool
+{
+    /**
+     * @var Reader
+     */
+    protected $reader;
+
+    public function __construct($ipdbPath = '')
+    {
+        if (empty($ipdbPath)) {
+            $ipdbPath = dirname(__DIR__). '/data/vipdata.ipdb';
+        }
+
+        $this->reader = new Reader($ipdbPath);
+    }
+
+    public function findMap($ip, $language = 'CN')
+    {
+        return $this->reader->findMap($ip, $language);
+    }
+}
